d97a6db7dc3df433d7019455e774129057a4c673
Diego T. Fialho <diego.tfialho@gmail.com>
2016-09-08 10:54:16 -0300

    Part 2

--- app/core/vendor.js
+++ app/core/vendor.js
@@ -1,6 +1,12 @@
 module.exports = function() {
-	/* styles */
-	require('../index.scss');
 	/* JS */
+	global.$ = global.jQuery = require('jquery');
+	require('node-lumx/node_modules/velocity-animate');
 	require('angular');
+	global.moment = require('node-lumx/node_modules/moment');
+	require('node-lumx');
+
+	/* Styles */
+	require('../index.scss');
+	require('../../node_modules/mdi/css/materialdesignicons.min.css');
 };
--- app/index.js
+++ app/index.js
@@ -1,8 +1,1 @@
-module.exports = angular.module('app', []);
-
-function printMessage(status='working') {
-	let message = 'ES6';
-	console.log(`${message} is ${status}`);
-}
-
-printMessage();
+module.exports = angular.module('app', ['lumx']);
